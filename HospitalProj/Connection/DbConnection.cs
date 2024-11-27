3cee4859a8bd3433720687da0fe3b80bcd58de0d
Andrey Turov <andrey.turoff@gmail.com>
2024-11-28 00:17:10 +0300

    pass

--- HospitalProj/Connection/DbConnection.cs
+++ HospitalProj/Connection/DbConnection.cs
@@ -10,7 +10,8 @@ namespace HospitalProj.Connection
     {
         private static string filePath = "Olena_hosp.accdb";
         //private static string con = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={filePath};Persist Security Info=False;";
-        private static string con = $"Host=localhost;Port=5432;Username=postgres;Password=password;Database=ISPsycho";
+        //private static string con = $"Host=localhost;Port=5432;Username=postgres;Password=password;Database=ISPsycho";
+        private static string con = $"Host=localhost;Port=5432;Username=postgres;Password=123;Database=ISPsycho";
 
         public static List<List<object>> DoSqlCommand(this string sqlQuery, int columns)
         {
